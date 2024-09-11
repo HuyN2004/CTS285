f04809b87bae0b6239937942a9b83175d4e3bc7d
HuyN2004 <103298928+HuyN2004@users.noreply.github.com>
2024-09-11 16:33:26 +0000

    update hello.py to route to another url

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -4,5 +4,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello from CTS285!<!p>"
+def index():
+    # three quotes for multiline strings
+    return """
+    <h3>Hello from CTS285!!!</h3>
+    <p> this is paragraph</p>
+    <a href="action">Click here</a>
+
+
+
+    """
+@app.route("/action")
+def action():
+    return "Hello form the action route!"
