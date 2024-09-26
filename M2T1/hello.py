a39e43014a06978f7729b4205592a2397e926f4d
HuyN2004 <103298928+HuyN2004@users.noreply.github.com>
2024-09-26 06:00:01 +0000

    A first cut at the template

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -2,15 +2,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 #Do any app specific setup here
 # for instance, loading a database
 
 @app.route("/")
 def index():
-    # find the template in /templates
-    name = "Huy"
-    return render_template("main_page.html", name=name)
+    return render_template("main_page.html")
 
 @app.route("/action")
 def action():
-    return "Hello form the action route!"
+    return "Hello from the action route!"
